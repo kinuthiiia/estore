559884b8574cbe4202021efa7b661f7783697bb3
Steve Kinuthia <skinuthia800@gmail.com>
2023-08-03 10:54:04 +0300

    done

--- components/refinementlist.jsx
+++ components/refinementlist.jsx
@@ -22,7 +22,10 @@ export default function CustomRefinementList(props) {
     <div>
       {items.map((item) => (
         <div className="flex space-x-4 p-2">
-          <a href={`/category/${item?.value}`} className="text-[0.8rem] block">
+          <a
+            href={`/category/${item?.value}`}
+            className=" uppercase text-[0.8rem] block"
+          >
             {item?.value}
           </a>
 
